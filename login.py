3a22553345280fdcb1c0b4d8bf7721580fb90d6c
Nicholas Smith <nicholas.smith@nicholassmith.me>
2018-08-09 15:11:42 -0500

    Added ability to register

--- login.py
+++ login.py
@@ -2,7 +2,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, flash, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators
 from wtforms.validators import Required, Length, ValidationError
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, \
@@ -23,6 +23,19 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Submit')
 
+class RegisterForm(FlaskForm):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+    submit = SubmitField('Register')
+    def validate_password(form, field):
+        if form.password.data != form.confirm.data:
+            flash('warning', 'Registration unsuccessful: Passwords must match')
+
 class CorgiForm(FlaskForm):
     certificate = StringField('Certificate Number', validators=[Required()])
     submit = SubmitField('Submit')
@@ -30,7 +43,6 @@ class CorgiForm(FlaskForm):
     def validate_certificate(form, field):
         if len(field.data) != 8:
             raise ValidationError('Certificate must be exactly 8 digits long.')
-            flash('invalid')
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -38,6 +50,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(16), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     cert = db.Column(db.String(128))
+    logged = db.Column(db.Integer)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -47,7 +60,7 @@ class User(UserMixin, db.Model):
 
     @staticmethod
     def register(username, password, cert):
-        user = User(username=username, cert=cert) 
+        user = User(username=username, cert=cert, logged=0) 
         user.set_password(password)
         db.session.add(user)
         db.session.commit()
@@ -56,6 +69,11 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User {0}>'.format(self.username)
 
+def logCert():
+    __tablename__ = 'users'
+    user = User.query.filter_by(username=current_user.username).first()
+    user.logged = 1
+    db.session.commit()
 
 @lm.user_loader
 def load_user(id):
@@ -70,14 +88,24 @@ def login():
         if user is None or not user.verify_password(form.password.data):
             return redirect(url_for('login', **request.args))
         login_user(user, form.remember_me.data)
+        flash('Logged in!', 'success')
         return redirect(request.args.get('next') or url_for('protected'))
     return render_template('login.html', form=form)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        User.register(form.username.data, form.password.data, '12345678')
+        return redirect(url_for('login'))
+        flash('Registration successful! Login with your newly created account')
+    return render_template('register.html', form=form)
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
+    flash('Successfully logged out', 'dark')
     return redirect(url_for('index'))
 
 
@@ -98,8 +126,7 @@ def protected():
     if form.validate_on_submit():
         if current_user.cert == form.certificate.data:
             valid = 1
-            flash("Correct!")
-            flash("Thanks for using!")
+            logCert()
     return render_template('protected.html', form=form, valid=valid)
 
 
